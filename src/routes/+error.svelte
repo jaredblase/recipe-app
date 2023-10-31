e7eb3fcf4c551c1edf1e854e75ef37db9013bb5a
Jared Sy <jaredblase@gmail.com>
2023-10-31 10:37:51 +0800

    update: homepage

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from '$app/stores'
+</script>
+
+<section>
+	<div class="container grid">
+		<p>{$page.status}</p>
+		<p>{$page.error?.message}</p>
+		<a href="/recipes">Click here to go back to our recipes</a>
+	</div>
+</section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,18 @@
 </script>
 
 <svelte:head>
-	<title>Enrollment System</title>
-	<meta name="description" content="mock enrollemnt system" />
+	<title>RecipeBook</title>
+	<meta name="description" content="A simple recipe storage app" />
 </svelte:head>
-<main>
+<header class="shadow-md fixed top-0 w-full bg-white">
+	<nav class="container h-14 flex items-center justify-between">
+		<a class="font-bold text-2xl" href="/">RecipeBook</a>
+		<ul class="flex items-center gap-x-8">
+			<li><a href="/recipes">Recipes</a></li>
+			<li><a href="/plans">Plans</a></li>
+		</ul>
+	</nav>
+</header>
+<main class="pt-14">
 	<slot />
 </main>
